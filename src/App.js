099569b1f373f90361ba304c5d2e4337968a3fe7
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-04 20:52:29 -0800

    content component built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg'
+// import logo from './logo.svg'
 import './App.css';
 import Title from './Title';
+import POPOSList from './POPOSList';
 
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
