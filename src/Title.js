c7b9538cd7b1946708652c0e80a49c8689569297
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-04 20:30:26 -0800

    [update] header component built

---
+++ src/Title.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Title.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+			<div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
+export default Title
+
+// JSX is an extention of the HTML language. It uses the XML language syntax
+// and in a React Project can be written alongside your regular Javascript.
