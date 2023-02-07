ec9ced3260843c9a752345d6cfad70cf4a44f618
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-06 21:18:08 -0800

    [update] organized files

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import { Outlet } from 'react-router-dom';
 
 import './App.css';
 import Title from './Title/Title';
+import Footer from './Footer/Footer';
 
 function App() {
   return (
     <div>
       <Title />
       <Outlet />
+      <Footer />
     </div>
   );
 }
