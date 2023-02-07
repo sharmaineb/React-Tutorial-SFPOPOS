c6088fb408c4743aecf4b95cd8853df2e317edcc
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-02-07 00:33:30 -0800

    [update] fixed style bug!

--- src/components/POPOSDetails/POPOSDetails.js
+++ src/components/POPOSDetails/POPOSDetails.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React from 'react'
+import { useParams } from 'react-router'
 import data from '../../sfpopos-data.js'
-import './POPOSDetails.css';
-import POPOSFeatureList from '../POPOSFeatureList.js';
-
+import POPOSFeatureList from '../POPOSFeatureList.js'
+import './POPOSDetails.css'
 
 function POPOSDetails(props) {
   const params = useParams()
   const { id } = params // Location index
   const { images, title, desc, hours, features, geo } = data[id]
-  
+
   return (
-    <div className="POPOSDetails">
-      <div className="POPOSDetails-image">
-        <img src={`${process.env.PUBLIC_URL}images/${images[0]}`} alt={title} />
+    <div className='POPOSDetails'>
+      <div className='POPOSDetails-image'>
+        <img src={`${process.env.PUBLIC_URL}/images/${images[0]}`} alt={title} />
       </div>
-      <div className="POPOSDetails-info">
-        <h1 className="POPOSDetails-title">{ title }</h1>
-        <p className="POPOSDetails-desc">{ desc }</p>
-        <p className="POPOSDetails-hours">{ hours }</p>
-        <p className="POPOSDetails-features">{ features }</p>
-        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
-        <POPOSFeatureList features={features}/>
-        <p className="POPOSDetails-geo">{ geo.lat } { geo.lon }</p>
+      <div className='POPOSDetails-info'>
+        <h1 className='POPOSDetails-title'>{ title }</h1>
+        <p className='POPOSDDetails-desc'>{ desc }</p>
+        <p className='POPOSDetails-hours'>{ hours }</p>
+        <POPOSFeatureList features={features} />
+        <p className='POPOSDetails-geo'>{ geo.lat } { geo.lon }</p>
       </div>
+
     </div>
   )
 }
+
 export default POPOSDetails
--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -1,31 +1,24 @@
-// import logo from './logo.svg';
-import './POPOSSpace.css';
-import { Link } from 'react-router-dom';
+import './POPOSSpace.css'
+import { Link } from 'react-router-dom'
 
 function POPOSSpace(props) {
     const { name, image, address, hours, id } = props
     return (
       <div className='POPOSSpace'>
-          <Link to={`/details/${id}`}>
-              <img src={`${process.env.PUBLIC_URL}/images/${image}`} 
-              width="300" 
-              height="300" 
-              alt="Hello"
-              />
+        <h1>
+          <Link to={`/details/${id}`} className='POPOSSpace-title'>
+            {name}
           </Link>
-          <h1>
-            <Link 
-              className="POPOSSpace-title"
-              to={`/details/${id}`}>
-              {name}
-            </Link>
-          </h1>
-          <div className="POPOSSpace-info">
-				    <div >{address}</div>
-				    <div>{hours}</div>
-			  </div>
+        </h1>
+        <Link to={`/details/${id}`}>
+          <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="350" height="300" alt="Hello" />
+        </Link>
+        <div className='POPOSSpace-info'>
+          <div>{address}</div>
+          <div className='hours'>{hours}</div>
+        </div>
       </div>
-  )
+    )
 }
 
 export default POPOSSpace
